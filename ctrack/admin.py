ce903e4a1d37c67a7e975d3ac1e25cf9e858348c
David Kent <davidkent@fastmail.com.au>
2016-09-20 19:04:20 +1000

    Basic config of admin app.

--- ctrack/admin.py
+++ ctrack/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from ctrack import models
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('when', 'description', 'amount', 'is_split', 'category', 'account')
+
+admin.site.register(models.Transaction, TransactionAdmin)
+admin.site.register(models.SplitTransaction, TransactionAdmin)
