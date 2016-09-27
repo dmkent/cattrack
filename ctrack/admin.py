3dda538dc309702a2596b21fc50f1e3cb304d08d
David Kent <davidkent@fastmail.com.au>
2016-09-27 17:11:48 +1000

    First cut a bills model.

--- ctrack/admin.py
+++ ctrack/admin.py
@@ -8,3 +8,5 @@ class TransactionAdmin(admin.ModelAdmin):
 admin.site.register(models.Transaction, TransactionAdmin)
 admin.site.register(models.SplitTransaction, TransactionAdmin)
 admin.site.register(models.PeriodDefinition)
+admin.site.register(models.Bill)
+admin.site.register(models.RecurringPayment)
--- ctrack/models.py
+++ ctrack/models.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 
 from dateutil.relativedelta import relativedelta
 from django.db import models
@@ -122,13 +122,13 @@ class PeriodDefinition(models.Model):
             start_date = end_date - relativedelta(years=1) - offset
 
             if self.anchor_date is None:
-                dates = pd.date_range(start_date, end_date, 
+                dates = pd.date_range(start_date, end_date,
                                       freq=self.frequency)
             else:
                 if pd.Timestamp(self.anchor_date) > start_date:
                     raise ValueError("unable to anchor periods")
-                    
-                dates = pd.date_range(self.anchor_date, end_date, 
+
+                dates = pd.date_range(self.anchor_date, end_date,
                                         freq=self.frequency)
                 dates = dates[dates >= np.datetime64(start_date)]
             self._index = dates
@@ -138,7 +138,7 @@ class PeriodDefinition(models.Model):
     def date_ranges(self):
         if self._ranges is None:
             dates = self.index
-            self._ranges = [(start, start_next - relativedelta(days=1)) 
+            self._ranges = [(start, start_next - relativedelta(days=1))
                             for start, start_next in zip(dates[:-1], dates[1:])]
         return self._ranges
 
@@ -174,3 +174,76 @@ class PeriodDefinition(models.Model):
                 "offset": 2,
             }
         ]
+
+
+class Bill(models.Model):
+    """A single bill to be paid."""
+    description = models.CharField(max_length=100)
+    due_date = models.DateField()
+    due_amount = models.DecimalField(max_digits=8, decimal_places=2)
+    fixed_amount = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    var_amount = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    paying_transactions = models.ManyToManyField("Transaction",
+                                                 related_name="pays_bill")
+    document = models.FileField(null=True)
+    series = models.ForeignKey('RecurringPayment', related_name="bills")
+
+    def __str__(self):
+        return "{} bill of ${:.2f} due on {}".format(
+            self.description,
+            self.due_amount,
+            self.due_date,
+        )
+
+
+class RecurringPayment(models.Model):
+    """A recurring bill series."""
+    name = models.CharField(max_length=100)
+    is_income = models.BooleanField(default=False)
+
+    def bills_as_series(self):
+        """Convert related Bill objects to time series."""
+        arr = np.array(self.bills.values_list('due_date', 'due_amount'))
+        return pd.Series(arr[:, 1], index=arr[:, 0])
+
+    def next_due_date(self):
+        """Calculate the due date of the next bill."""
+        data = self.bills_as_series()
+        days_between = [td.total_seconds() / 86400 for td in data.index.values[1:] - data.index.values[:-1]]
+        mean_days = np.mean(days_between)
+        last_due = data.index[-1]
+        if mean_days < 10:
+            freq = 'W'
+            delta = timedelta(days=last_due.dayofweek)
+        elif mean_days < 15:
+            freq = '2W'
+            delta = timedelta(days=last_due.dayofweek)
+        elif mean_days < 33:
+            freq = 'MS'
+            delta = timedelta(days=last_due.day - 1)
+        elif mean_days < 65:
+            freq = '2MS'
+            delta = timedelta(days=last_due.day - 1)
+        elif mean_days < 100:
+            freq = 'QS'
+            delta = timedelta(days=last_due.day - 1)
+        elif mean_days < 190:
+            freq = '6MS'
+            delta = timedelta(days=last_due.day - 1)
+        elif mean_days < 367:
+            freq = 'AS'
+            delta = timedelta(days=last_due.dayofyear)
+        else:
+            freq = None
+            delta = 0
+
+        if freq is not None:
+            offset = pd.datetools.to_offset(freq)
+            return data.index[-1] + offset + delta
+        return
+
+    def __str__(self):
+        if self.is_income:
+            return "Income: {}".format(self.name)
+        else:
+            return "Bill: {}".format(self.name)
