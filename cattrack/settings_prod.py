dc2db9c2d51ac882161cfc5a8def458fb08150f3
David Kent <davidkent@fastmail.com.au>
2020-04-13 09:19:23 +1000

    Add logging config

--- cattrack/settings_prod.py
+++ cattrack/settings_prod.py
@@ -126,6 +126,20 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = []
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
+
 CTRACK_CATEGORISER = 'SklearnCategoriser'
 CTRACK_CATEGORISER_FILE = os.path.join(BASE_DIR, 'categoriser.pkl')
 
